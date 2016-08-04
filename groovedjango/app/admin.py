55735ca384bd14d4739c28c2a4076fbe27793a06
Mauricio Ribeiro <mauricioribeiro1992@gmail.com>
2016-08-03 21:37:00 -0300

    primeiro commit
    models criados e alguns artistas, albuns e categorias adicionadas via
    shell

---
+++ groovedjango/app/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from app.models import *
+
+# Register your models here.
+admin.site.register(Categoria)
+admin.site.register(Artista)
+admin.site.register(Album)
+admin.site.register(Musica)
---
+++ groovedjango/app/models.py
@@ -0,0 +1,37 @@
+from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
+
+# estas funcoes sao usadas nos args "upload_to" para fazer migrations, uma vez que o Django nao faz migrations com funcoes anonimas (lambdas)
+def get_artista_path(instance, filename): return 'artistas/{0}_{1}'.format(instance.nome, filename)
+def get_album_path(instance, filename): return 'albuns/{0}_{1}'.format(instance.nome, filename)
+
+class Categoria(models.Model):
+	nome = models.CharField(max_length = 100)
+
+	def __str__(self):
+		return self.nome
+
+class Artista(models.Model):
+	nome = models.CharField(max_length = 100)
+	foto = models.ImageField(upload_to = get_artista_path, max_length = 100)
+
+	def __str__(self):
+		return self.nome
+
+class Album(models.Model):
+	nome = models.CharField(max_length = 100)
+	foto = models.ImageField(upload_to = get_album_path, max_length = 100)
+	ano = models.IntegerField(validators = [MinValueValidator(1899), MaxValueValidator(2999)])
+	categoria = models.ManyToManyField(Categoria)
+	artista = models.ForeignKey(Artista, on_delete=models.PROTECT)
+
+	def __str__(self):
+		return '{0} ({1})'.format(self.nome, self.ano)
+
+class Musica(models.Model):
+	nome = models.CharField(max_length = 100)
+	duracao = models.DurationField()
+	album = models.ForeignKey(Album, on_delete=models.CASCADE)
+
+	def __str__(self):
+		return '{0} - {1}'.format(self.nome, self.duracao)
