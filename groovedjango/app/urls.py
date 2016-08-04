4a0b2f5825627b3d48444a0441733808f7499ecc
Mauricio Ribeiro <mauricioribeiro1992@gmail.com>
2016-08-04 00:02:30 -0300

    adicionando bootstrap

--- groovedjango/app/urls.py
+++ groovedjango/app/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^artistas/$', views.artistas_listar, name='artistas_listar'),
 ]
--- groovedjango/app/views.py
+++ groovedjango/app/views.py
@@ -4,3 +4,6 @@ from django.shortcuts import render
 
 def index(request):
 	return render(request, 'index.html', {})
+
+def artistas_listar(request):
+	return render(request,'artistas/listar.html')
