6c3e99b64fa72de603c7acc9fc8380aa42cf6f0b
Mauricio Ribeiro <mauricioribeiro1992@gmail.com>
2016-08-03 21:49:24 -0300

    adicionando media url aos padroes do urls.py
    modo debug true

--- groovedjango/groovedjango/urls.py
+++ groovedjango/groovedjango/urls.py
@@ -13,10 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url, include
+from django.conf.urls import url, include, patterns
 from django.contrib import admin
+from django.conf import settings
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^groovedjango/', include('app.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += patterns('', (r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}))
